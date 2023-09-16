b522a5e47a819a8d3a797bccf855ae036c3fa0b5
CosmoJenkinsQA <cosmo.jenkinsqa@gmail.com>
2023-09-16 22:21:23 +0300

    done

---
+++ src/test/java/com/example/MainPageTest.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private static WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    @BeforeEach
+    public void wellcome(){
+        System.out.println("Thanks for watching my test ");
+    }
+
+    @Test
+    public void search() throws InterruptedException {
+
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys("Test");
+        Thread.sleep(3000);
+        searchField.submit();
+        Thread.sleep(3000);
+
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+        assertEquals("Test", searchPageField.getAttribute("value"));
+
+    }
+}
+
+
